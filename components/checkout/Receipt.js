f0d71775ef5888ac1836293c2fdd85903450e4a1
abda-torey <abdatorey@gmail.com>
2023-11-09 19:01:00 +0000

    modified receipt page

--- components/checkout/Receipt.js
+++ components/checkout/Receipt.js
@@ -2,6 +2,11 @@
 import React, { useEffect, useState } from "react";
 import { format, utcToZonedTime } from "date-fns-tz";
 import Image from "next/image";
+import {
+  
+  useStripe,
+  
+} from "@stripe/react-stripe-js";
 
 const formatDateToET = (dateStr) => {
   const easternTime = utcToZonedTime(dateStr, "America/New_York");
@@ -13,6 +18,8 @@ const formatDateToET = (dateStr) => {
 const Receipt = () => {
   const [orderData, setOrderData] = useState(null);
   const [error, setError] = useState(null);
+  const stripe = useStripe();
+  
   useEffect(() => {
     const storedOrderId = localStorage.getItem("orderId");
     const id = storedOrderId.replace(/"/g, ""); // Strip quotation marks
@@ -41,7 +48,39 @@ const Receipt = () => {
   const createdAtValue = orderData && orderData.createdAt;
   const orderId = orderData && orderData.orderId;
   console.log(createdAtValue);
+  useEffect(() => {
+    if (!stripe) {
+      return;
+    }
 
+    const clientSecret = new URLSearchParams(window.location.search).get(
+      "payment_intent_client_secret"
+    );
+
+    if (!clientSecret) {
+      return;
+    }
+
+    stripe.retrievePaymentIntent(clientSecret).then(({ paymentIntent }) => {
+      switch (paymentIntent.status) {
+        case "succeeded":
+          setMessage("Payment succeeded!");
+          break;
+        case "processing":
+          setMessage("Your payment is processing.");
+          break;
+        case "requires_payment_method":
+          setMessage("Your payment was not successful, please try again.");
+          break;
+        default:
+          setMessage("Something went wrong.");
+          break;
+      }
+      // Access the orderId from PaymentIntent's metadata
+      const orderId = paymentIntent.metadata.orderId;
+      console.log(`Order ID: ${orderId}`);
+    });
+  }, [stripe]);
   return (
     <div className=" mt-24  mb-8 flex flex-col items-center justify-center text-black mx-2 md:mx-0">
       <div className=" bg-customColor w-full md:w-[50%] p-4">
@@ -62,27 +101,37 @@ const Receipt = () => {
             <h2 className="text-xl font-FuturaLight mb-4">
               Detailed Order Receipt
             </h2>
-            <p className="mb-4 font-FuturaLight text-xs">Order Number: {orderId}</p>
+            <p className="mb-4 font-FuturaLight text-xs">
+              Order Number: {orderId}
+            </p>
             <p className="mb-4 font-FuturaLight text-xs">
               Order Date: {createdAtValue && formatDateToET(createdAtValue)}
             </p>
-           
-            
-            <p className="mb-4 font-FuturaLight tracking-widest">Ship to John Newman</p>
 
-            <h3 className="text-lg font-FuturaMedium tracking-widest mb-4">Shipping Address</h3>
-            
-            <span className="mb-2 font-FuturaLight text-sm tracking-widest">John Newman</span><br></br>
-            <span className="mb-2 font-FuturaLight text-sm tracking-widest">CHESTNUT ST</span><br></br>
+            <p className="mb-4 font-FuturaLight tracking-widest">
+              Ship to John Newman
+            </p>
+
+            <h3 className="text-lg font-FuturaMedium tracking-widest mb-4">
+              Shipping Address
+            </h3>
+
+            <span className="mb-2 font-FuturaLight text-sm tracking-widest">
+              John Newman
+            </span>
+            <br></br>
+            <span className="mb-2 font-FuturaLight text-sm tracking-widest">
+              CHESTNUT ST
+            </span>
+            <br></br>
             <span className="mb-2 font-FuturaLight text-sm tracking-widest">
               SACRAMENTO, CA,94203
             </span>
             <p className="mb-8 font-FuturaLight text-sm">+4407729966565</p>
           </div>
-          
+
           <div className="w-full md:w-[60%] pl-10   hidden md:flex flex-col">
             <div className=" flex  ">
-              
               <div className="flex-grow overflow-y-auto  p-0 items-start justify-start  h-[140px]  ">
                 {orderData?.products?.map((product, productIdx) => (
                   <div className="flex mt-7  " key={productIdx}>
@@ -124,7 +173,9 @@ const Receipt = () => {
             <div className=" hidden md:flex ">
               <div>
                 <div className=" mt-4 w-36 ">
-                  <h3 className=" font-FuturaMedium tracking-widest  text-sm">Shipping Method</h3>
+                  <h3 className=" font-FuturaMedium tracking-widest  text-sm">
+                    Shipping Method
+                  </h3>
                   <p className="pl-1 mt-1 font-FuturaLight tracking-widest text-xs">
                     standard fee
                   </p>
@@ -147,8 +198,12 @@ const Receipt = () => {
                   <p className=" font-FuturaLight text-[13px] tracking-widest">
                     Merchandize $50.00
                   </p>
-                  <p className=" font-FuturaLight text-[13px] tracking-widest">Shipping $8.99</p>
-                  <p className=" font-FuturaLight text-[13px] tracking-widest">Tax $0.00</p>
+                  <p className=" font-FuturaLight text-[13px] tracking-widest">
+                    Shipping $8.99
+                  </p>
+                  <p className=" font-FuturaLight text-[13px] tracking-widest">
+                    Tax $0.00
+                  </p>
                   <h3 className=" font-FuturaMedium text-[13px] tracking-widest">
                     Order Total: $50.00
                   </h3>
@@ -158,7 +213,9 @@ const Receipt = () => {
           </div>
         </div>
       </div>
-      <button className=" block md:hidden font-FuturaMedium tracking-widest bg-black text-white border p-4 w-44 mt-4">Print Receipt</button>
+      <button className=" block md:hidden font-FuturaMedium tracking-widest bg-black text-white border p-4 w-44 mt-4">
+        Print Receipt
+      </button>
     </div>
   );
 };
